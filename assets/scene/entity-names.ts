ee29070e8ef705c104fb7145e723e5d99cc5f7e2
nearnshaw <earnshaw.nico@gmail.com>
2025-05-06 10:17:46 +0200

    add streaming via Admin Tools

---
+++ assets/scene/entity-names.ts
@@ -0,0 +1,11 @@
+// Auto-generated entity names from the scene
+
+
+/**
+ * Object containing all entity names in the scene for autocomplete support.
+ */
+export enum EntityNames {
+  Admin_Tools = "Admin Tools",
+  Video_Screen = "Video Screen",
+  base_theatre_glb = "base_theatre.glb",
+} 
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,15 @@
 import { MeshRenderer, engine, Material, Transform, VideoPlayer, pointerEventsSystem, InputAction, MeshCollider } from '@dcl/sdk/ecs'
 import { Color4, Quaternion } from '@dcl/sdk/math'
+import { EntityNames } from '../assets/scene/entity-names'
+import { getTriggerEvents, getActionEvents } from '@dcl/asset-packs/dist/events'
+import { TriggerType } from '@dcl/asset-packs'
 
 export function main() {
-	const videoPlayerEntity = engine.addEntity()
-	VideoPlayer.create(videoPlayerEntity, {
-		src: 'https://player.vimeo.com/external/552481870.m3u8?s=c312c8533f97e808fccc92b0510b085c8122a875',
-		playing: true,
-		volume: 1.0
-	})
 
-	const screen = engine.addEntity()
-	Transform.create(screen, {
-		position: { x: 117, y: 20.5, z: 80 },
-		scale: { x: 38.4, y: 21.6, z: 1 },
-		rotation: Quaternion.fromEulerDegrees(0, 90, 0)
-	})
-	MeshRenderer.setPlane(screen)
-	MeshCollider.setPlane(screen)
-	Material.setBasicMaterial(screen, {
-		texture: Material.Texture.Video({ videoPlayerEntity: videoPlayerEntity })
-	})
+  // fetch screens from editor
+  const screen = engine.getEntityOrNullByName(EntityNames.Video_Screen)
 
+  if (screen) {
+  }
 
-
-	pointerEventsSystem.onPointerDown(
-		{
-			entity: screen,
-			opts: {
-				button: InputAction.IA_POINTER,
-				hoverText: 'Play/pause'
-			}
-		},
-		() => {
-			const videoPlayer = VideoPlayer.getMutable(videoPlayerEntity)
-			videoPlayer.playing = !videoPlayer.playing
-		}
-	)
 }
